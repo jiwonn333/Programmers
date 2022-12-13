13e8d172447b5c6f3467228cc3252770f16fd8e2
myWon <sjwon113@naver.com>
2022-12-13 16:56:13 +0900

    [test2. cyclic_rotation]

--- app/src/test/java/com/example/testcodeinjava/TestCode.java
+++ app/src/test/java/com/example/testcodeinjava/TestCode.java
@@ -2,12 +2,33 @@ package com.example.testcodeinjava;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class TestCode {
 
     @Test
     public void testCode() {
         // test1
         System.out.println("test1 ::: binary_gap : " + binaryGap(8806));
+        System.out.println("#test2 ::: cyclic_rotation : "
+                + Arrays.toString(cyclicRotation(new int[]{1, 2, 3, 4}, 2)));
+    }
+
+    // test2: cyclic_rotation
+    public int[] cyclicRotation(int[] A, int k) {
+        int length = A.length;
+        int temp;
+        if (length == 0) {
+            return A;
+        }
+        for (int i = 0; i < k; i++) {
+            temp = A[length - 1];
+            for (int j = length - 1; j > 0; j--) {
+                A[j] = A[j - 1];
+            }
+            A[0] = temp;
+        }
+        return A;
     }
 
     // test1: Binary_Gap
