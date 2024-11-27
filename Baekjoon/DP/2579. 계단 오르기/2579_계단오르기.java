9a39a65e5843d6dbd3921bc4a9177dad35c18b9b
happy jiwon <sjwon113@naver.com>
2024-11-28 01:42:48 +0900

    Solve: 백준 2597 계단 오르기

---
+++ Baekjoon/DP/2579. 계단 오르기/2579_계단오르기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); // N개의 계단
+        int[] stairs = new int[N + 1]; // 계단 배열 생성
+
+        // 각 계단에 점수 입력
+        for (int i = 1; i <= N; i++) {
+            stairs[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] score = new int[N + 1];
+        // 기저조건 재생성
+        score[1] = stairs[1];
+        score[2] = stairs[1] + stairs[2];
+        for (int i = 3; i <= N; i++) {
+            score[i] = Math.max(score[i - 2], score[i - 3] + stairs[i - 1]) + stairs[i];
+        }
+
+        System.out.println(score[N]);
+    }
+}
