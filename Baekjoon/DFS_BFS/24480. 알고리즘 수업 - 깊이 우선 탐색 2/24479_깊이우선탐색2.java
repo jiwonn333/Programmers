f308989b539c19a70b635b1fcfa16f2d0d3b8851
happy jiwon <sjwon113@naver.com>
2025-01-22 15:16:01 +0900

    Solve: 백준 24480 알고리즘 수업 - 깊이우선 탐색 2

---
+++ Baekjoon/DFS_BFS/24480. 알고리즘 수업 - 깊이 우선 탐색 2/24479_깊이우선탐색2.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int count = 0;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        // N, M, R
+        int n = Integer.parseInt(st.nextToken()); // 정점의 개수
+        int m = Integer.parseInt(st.nextToken()); // 간선의 개수
+        int r = Integer.parseInt(st.nextToken()); // 시작점
+
+
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int left = Integer.parseInt(st.nextToken());
+            int right = Integer.parseInt(st.nextToken());
+            graph.get(left).add(right);
+            graph.get(right).add(left);
+        }
+
+        // 내림차순 정렬
+        for (int i = 1; i < graph.size(); i++) {
+            Collections.sort(graph.get(i), Collections.reverseOrder());
+        }
+
+        int[] visited = new int[n + 1];
+
+        count = 1; // 1부터 시작
+        dfs(graph, visited, r);
+
+        for (int i = 1; i < visited.length; i++) {
+            sb.append(visited[i] + "\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static void dfs(List<List<Integer>> graph, int[] visited, int startNode) {
+        visited[startNode] = count;
+
+        for (int nextNode : graph.get(startNode)) {
+            if (visited[nextNode] == 0) {
+                count++;
+                dfs(graph, visited, nextNode);
+            }
+        }
+    }
+}
